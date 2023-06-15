a08932b281a6c9cce12186b53a323875a9232547
LudmiAbril <ludmila.pereyra543@gmail.com>
2023-06-14 22:51:32 -0300

    inscripcion-forms

--- js/inscripcion-agregar-fila.js
+++ js/inscripcion-agregar-fila.js
@@ -1,16 +1,33 @@
 
-function agregarFila() {
+let botonagregarFila = document.getElementById('boton-agregar-persona');
+let contenedor = document.getElementById('filas');
 
+botonagregarFila.addEventListener("click", function(event) {
+    event.preventDefault();
     let filaPrevia = document.querySelector('.fila-campo');
     let filaNueva = filaPrevia.cloneNode(true);
+    contenedor.appendChild(filaNueva);
+
+    let botonBorrar = filaNueva.getElementsByClassName('boton-borrar');
+
+    for (var i = 0; i < botonBorrar.length; i++) {
+        botonBorrar[i].addEventListener('click', function (event) {
+            event.preventDefault();
+            let fila = this.parentNode;
+            if (fila !== contenedor.firstElementChild) {
+                contenedor.removeChild(this.parentNode);
+            }
+        });
     
-    document.getElementById('filas').appendChild(filaNueva);
-
-}
-
-let botonagregarFila = document.getElementById('boton-agregar-persona');
-
-botonagregarFila.addEventListener("click", (e) => {
-    e.preventDefault();
-    agregarFila();
+    }
 });
+
+
+
+
+
+
+
+
+
+
--- js/inscripcion-eliminar-fila.js
+++ js/inscripcion-eliminar-fila.js
@@ -1,22 +1,16 @@
-function borrarFila() {
-    let fila = document.querySelector('.fila-campo');
-    let filaBorrar = fila.parentNode();
-
-    filaBorrar.removeChild()
-};
-
-
 let botonBorrar = document.getElementsByClassName('boton-borrar');
-let contenedor = document.getElementById('filas');
+let padre = document.getElementById('filas');
 
-for (var i = botonBorrar.length - 1; i > 1; i--) {
-
-    botonBorrar[i].addEventListener('click', (e) => {
-        e.preventDefault();
-        contenedor.removeChild(botonBorrar[i].parentNode);
-// buscar la forma de que se pueda con nuevos
+for (var i = 0; i < botonBorrar.length; i++) {
+    botonBorrar[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        let fila = this.parentNode;
+        if (fila !== padre.firstElementChild) {
+            padre.removeChild(this.parentNode);
+        }
     });
 
 
 }
+
 
