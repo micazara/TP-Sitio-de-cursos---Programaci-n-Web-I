87c7b079526f462f0967018a833555a95991ac32
Celena Moscovich <celena.ark@gmail.com>
2023-06-29 17:35:21 -0300

    jsjs

--- js/formulario.js
+++ js/formulario.js
@@ -10,82 +10,96 @@ Cambios en la página de formulario de contacto
 */
 
 let botonEnviar = document.querySelector("#boton-enviar-form");
-let mensaje = document.querySelector("#mensaje")
+let mensaje = document.querySelector("#mensaje");
+let error;
+let mensajesError = "";
 
+//CONTADOR
+let caracteresIngresadosMensaje = document.querySelector(
+  "#caracteresIngresados"
+);
+let caracteresRestantesMensaje = document.querySelector("#caracteresRestantes");
+let caracteresActuales = document.getElementById("consulta");
+
+//funcion dentro de submit
 botonEnviar.addEventListener("click", function (event) {
-    event.preventDefault()
+  event.preventDefault();
 
+  let form = document.querySelector("#formulario-consulta");
+  error = false;
+  let nombreApellido = document.querySelector("#nombre_y_apellido").value;
+  let email = document.querySelector("#mail").value;
+  let tel = document.querySelector("#telefono").value;
 
-    let form = document.querySelector("#formulario-consulta");
-    let error = false;
-    let mensajesError = "";
+  //CAPTURAR TEXTAREA
 
-    let nombreApellido = document.querySelector("#nombre_y_apellido").value;
-    let email = document.querySelector("#mail").value;
-    let tel = document.querySelector("#telefono").value
+  let cantidadActual = caracteresActuales.value.length;
+  let maxCaracteres = 1000;
 
-    // ○ Mail validado con expresión regular.
-    let regexEmail = /^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/;
+  // ○ Mail validado con expresión regular.
+  let regexEmail = /^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/;
 
-    // ○ El teléfono se debe validar el formato de 8 dígitos e incluir un guión medio entre el cuarto 
-    // y quinto número en caso de que el usuario lo ingrese, si no lo ingresa no se deben realizar validaciones.
+  // ○ El teléfono se debe validar el formato de 8 dígitos e incluir un guión medio entre el cuarto
+  // y quinto número en caso de que el usuario lo ingrese, si no lo ingresa no se deben realizar validaciones.
 
-    let regexNumeros = /^\d{4}-?\d{4}$/;
+  let regexNumeros = /^\d{4}-?\d{4}$/;
 
-    // ○ Consulta, se debe limitar a 1000 caracteres y mostrar la cantidad de caracteres ingresados y resantes en tiempo real.
- 
-    // V A L I D A C I O N
+  // ○ Consulta, se debe limitar a 1000 caracteres y mostrar la cantidad de caracteres ingresados y resantes en tiempo real.
 
-    // N O M B R E  Y  A P E L L I D O*/
+  // V A L I D A C I O N
 
-    if (nombreApellido == "") {
-        error = true;
-        mensajesError += "<p>Ingresá tu nombre y apellido</p>"
-    }
+  // N O M B R E  Y  A P E L L I D O*/
 
-    // E M A I L
-    if (!(regexEmail.test(email))) {
-        error = true;
-        mensajesError += " <p>Ingresá un email válido</p> "
-    }
+  if (nombreApellido == "") {
+    error = true;
+    mensajesError += "<p>Ingresá tu nombre y apellido</p>";
+  }
 
-    // T E L E F O N O
-    // testea que el regex se cumpla en tel
-    if (!(regexNumeros.test(tel))) {
-        error = true;
-        mensajesError += " <p>Ingresá un formato de telefono válido</p> "
-    }
+  // E M A I L
+  if (!regexEmail.test(email)) {
+    error = true;
+    mensajesError += " <p>Ingresá un email válido</p> ";
+  }
 
-    if (error) {
-        //muestre los errores
-        mensaje.innerHTML = mensajesError;
-    } else {
-        // form.submit();
+  // T E L E F O N O
+  // testea que el regex se cumpla en tel
+  if (!regexNumeros.test(tel)) {
+    error = true;
+    mensajesError += " <p>Ingresá un formato de telefono válido</p> ";
+  }
 
-        let modalFormularioExitoso = document.getElementById("modal-formulario-exitoso");
-        let tituloModalContacto = document.getElementById("titulo-modal-contacto");
-        tituloModalContacto.textContent = "Los carpis te saludan...estamos viendo tu consulta";
-        modalFormularioExitoso.style.display = "block";
+  if (cantidadActual> 1000 || cantidadActual == 0) {
+    error = true;
+    mensajesError +=
+      " <p>Los carpinchos queremos un mensaje, RELLENA EL CAMPO</p> ";
+  }
+  if (error) {
+    //muestre los errores
+    mensaje.innerHTML = mensajesError;
+  } else {
+    // form.submit();
 
-        let cerrarModal = document.querySelector(".cerrar-modal-formulario");
+    let modalFormularioExitoso = document.getElementById(
+      "modal-formulario-exitoso"
+    );
+    let tituloModalContacto = document.getElementById("titulo-modal-contacto");
+    tituloModalContacto.textContent =
+      "Los carpis te saludan...estamos viendo tu consulta";
+    modalFormularioExitoso.style.display = "block";
 
-        cerrarModal.addEventListener("click", (e) => {
-            modalFormularioExitoso.style.display = "none";
+    let cerrarModal = document.querySelector(".cerrar-modal-formulario");
 
-        })
+    cerrarModal.addEventListener("click", (e) => {
+      modalFormularioExitoso.style.display = "none";
+    });
+  }
+});
 
-
-
-    }
-
-
-
-
-
-
-
-
-
-})
-
-
+//FUNCION CUENTA
+caracteresActuales.addEventListener("keyup", () => {
+  let cantidadActual = parseInt(caracteresActuales.value.length);
+  let maxCaracteres = 1000;
+  let caracteresRestantes = maxCaracteres - cantidadActual;
+  caracteresIngresadosMensaje.innerHTML = `${cantidadActual} caracteres`;
+  caracteresRestantesMensaje.innerHTML = `${caracteresRestantes} caracteres`;
+});
